83e28406894e951a91656ca19037bcb9c3fb65a9
ahnhyowon <ahnhw2449@gmail.com>
2025-08-10 16:56:18 +0900

    launch 파일 수정

--- turtlebot3_autorace_detect/launch/detect_lane.launch.py
+++ turtlebot3_autorace_detect/launch/detect_lane.launch.py
@@ -26,8 +26,6 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    pkg_share = get_package_share_directory('turtlebot3_autorace_detect')
-
     calibration_mode_arg = DeclareLaunchArgument(
         'calibration_mode',
         default_value='False',
@@ -36,12 +34,11 @@ def generate_launch_description():
     calibration_mode = LaunchConfiguration('calibration_mode')
 
     detect_param = os.path.join(
-        pkg_share,
+        get_package_share_directory('turtlebot3_autorace_detect'),
         'param',
         'lane',
         'lane.yaml'
         )
-    param_file = os.path.join(pkg_share, 'param', 'traffic_light', 'traffic_light.yaml')
 
     detect_lane_node = Node(
         package='turtlebot3_autorace_detect',
@@ -63,40 +60,8 @@ def generate_launch_description():
             ('/detect/image_output_sub2/compressed', '/detect/image_yellow_lane_marker/compressed')
         ]
     )
-    detect_traffic_light_node = Node(
-        package='turtlebot3_autorace_detect',
-        executable='detect_traffic_light',
-        name='detect_traffic_light',
-        output='screen',
-        parameters=[
-            param_file,
-            {'is_calibration_mode': calibration_mode}
-        ],
-        remappings=[
-            ('/detect/image_input', '/camera/image_compensated'),
-            ('/detect/image_input/compressed', '/camera/image_compensated/compressed'),
-            ('/detect/image_output', '/detect/image_traffic_light'),
-            ('/detect/image_output/compressed', '/detect/image_traffic_light/compressed'),
-            ('/detect/image_output_sub1', '/detect/image_red_light'),
-            ('/detect/image_output_sub1/compressed', '/detect/image_red_light/compressed'),
-            ('/detect/image_output_sub2', '/detect/image_yellow_light'),
-            ('/detect/image_output_sub2/compressed', '/detect/image_yellow_light/compressed'),
-            ('/detect/image_output_sub3', '/detect/image_green_light'),
-            ('/detect/image_output_sub3/compressed', '/detect/image_green_light/compressed'),
-        ],
-    )
-    detect_human_node = Node(
-        package='turtlebot3_autorace_detect',
-        executable='person_detector',
-        name='person_detector',
-        output='screen',
-        remappings=[
-            ('/detect/human', '/control/human'),
-        ],
-    )
+
     return LaunchDescription([
         calibration_mode_arg,
         detect_lane_node,
-        detect_traffic_light_node,
-        detect_human_node
     ])
