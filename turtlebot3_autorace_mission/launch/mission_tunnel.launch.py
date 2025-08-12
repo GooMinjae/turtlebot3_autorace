6647944b15140580a6c830f75ecb7a3c8664f479
ahnhyowon <ahnhw2449@gmail.com>
2025-08-12 18:28:19 +0900

    mission_tunnel => 사람인식 launch

--- turtlebot3_autorace_mission/launch/mission_tunnel.launch.py
+++ turtlebot3_autorace_mission/launch/mission_tunnel.launch.py
@@ -20,44 +20,78 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    nav2_pkg_share = get_package_share_directory('turtlebot3_navigation2')
+    pkg_share = get_package_share_directory('turtlebot3_autorace_detect')
 
-    param_file = os.path.join(
-        get_package_share_directory('turtlebot3_autorace_mission'),
+    detect_param = os.path.join(
+        pkg_share,
         'param',
-        'navigation.yaml'
-    )
-    map_file = os.path.join(
-        get_package_share_directory('turtlebot3_autorace_mission'),
-        'map',
-        'map.yaml'
+        'lane',
+        'lane.yaml'
     )
 
-    nav2_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(nav2_pkg_share, 'launch', 'navigation2.launch.py')
-        ),
-        launch_arguments={
-            'use_sim_time': 'True',
-            'map': map_file
-        }.items()
-    )
-
-    mission_tunnel_node = Node(
+    avoid_object_node = Node(
         package='turtlebot3_autorace_mission',
-        executable='mission_tunnel',
-        name='mission_tunnel',
+        executable='avoid_construction',
+        name='avoid_construction',
+        output='screen'
+    )
+
+    detect_lane_node = Node(
+        package='turtlebot3_autorace_detect',
+        executable='detect_lane',
+        name='detect_lane',
         output='screen',
-        parameters=[param_file]
+        parameters=[
+            {'is_detection_calibration_mode': True},
+            detect_param
+        ],
+        remappings=[
+            ('/detect/image_input', '/camera/image_projected'),
+            ('/detect/image_input/compressed', '/camera/image_projected/compressed'),
+            ('/detect/image_output', '/detect/image_lane'),
+            ('/detect/image_output/compressed', '/detect/image_lane/compressed'),
+            ('/detect/image_output_sub1', '/detect/image_white_lane_marker'),
+            ('/detect/image_output_sub1/compressed', '/detect/image_white_lane_marker/compressed'),
+            ('/detect/image_output_sub2', '/detect/image_yellow_lane_marker'),
+            ('/detect/image_output_sub2/compressed', '/detect/image_yellow_lane_marker/compressed')
+        ]
+    )
+
+
+
+
+
+    control_node = Node(
+            package='turtlebot3_autorace_mission',
+            executable='control_lane',
+            name='control_lane',
+            output='screen',
+            remappings=[
+                ('/control/lane', '/detect/lane'),
+                ('/control/cmd_vel', '/cmd_vel')
+            ]
+        )
+
+    person_node = Node(
+            package='turtlebot3_autorace_detect',
+            executable='person_detector',
+            name='person_detector',
+            output='screen',
+            remappings=[
+            ]
     )
 
     return LaunchDescription([
-        nav2_launch,
-        mission_tunnel_node,
+        avoid_object_node,
+        detect_lane_node,
+        # detect_traffic_light_node,
+        person_node,
+        # detect_sign_node,
+        # detect_speed_node,
+        control_node,
+
     ])
