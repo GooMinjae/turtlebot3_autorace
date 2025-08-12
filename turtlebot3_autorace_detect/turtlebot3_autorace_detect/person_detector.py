2e8f42831f5fc3912d1cbeeb3ae2faeefef653a0
leejiiyoon <dldnrwls156@gmail.com>
2025-08-12 21:03:21 +0900

    사람인식테스트-1

--- turtlebot3_autorace_detect/turtlebot3_autorace_detect/person_detector.py
+++ turtlebot3_autorace_detect/turtlebot3_autorace_detect/person_detector.py
@@ -34,7 +34,7 @@ class PersonDetector(Node):
         self.yolo_person_class_id = 0
         self.yolo_box_area_stop_ratio = 0.03   # ROI 면적 대비 person 박스 합계가 이 비율↑면 STOP
         self.yolo_box_area_slow_ratio = 0.03   # ROI 면적 대비 person 박스 합계가 이 비율↑면 SLOW
-        self.yolo_stop_on_any_person = True    # ROI 내 사람이 보이면(면적 무관) SLOW 트리거
+        self.yolo_stop_on_any_person = False    # ROI 내 사람이 보이면(면적 무관) SLOW 트리거
 
         # [DEVICE] 기본 CPU 강제 (MX250 CUDA 커널 불일치 방지)
         self.yolo_device = 'cpu'  # 필요 시 'cuda:0' 로 변경
